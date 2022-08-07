e6d6d749e91b7b9ea394298f128276b638829699
Ivo Pugliese <elsoft74@gmail.com>
2022-08-07 16:33:01 +0200

    aggiunto noindex

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+    <meta name="robots" content="noindex">
     <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
     <script src="js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"></script>
     <script type="text/javascript" src="https://oss.sheetjs.com/sheetjs/xlsx.full.min.js"></script>
