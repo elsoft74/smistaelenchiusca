13c5b620572c5b70a4a35ef4986259a30eba0bc4
Ivo Pugliese <elsoft74@gmail.com>
2022-09-17 07:14:16 +0200

    imap list

--- test.php
+++ test.php
@@ -1,3 +1,29 @@
 <?php
-    phpinfo();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// use PHPMailer\PHPMailer\PHPMailer;
+//     use PHPMailer\PHPMailer\Exception;
+//     use PHPMailer\PHPMailer\SMTP;
+//     use PHPMailer\PHPMailer\IMAP;
+    include_once("config/config.php");
+
+    $path = "{".SENDERSERVER.":".SENDERPORT."}"; // [INBOX]/Sent Mail
+    $imapStream = imap_open($path, SENDERUSERNAME, SENDERPASSWORD);
+    // var_dump($imapStream);
+    // imap_close($imapStream);
+    
+
+    $folders = imap_listmailbox($imapStream, $path, "*");
+
+if ($folders == false) {
+    echo "Call failed<br />\n";
+} else {
+    foreach ($folders as $val) {
+        echo $val . "<br />\n";
+    }
+}
+
+// phpinfo();
+
 ?>
